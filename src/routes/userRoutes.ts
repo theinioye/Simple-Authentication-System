cfd72f0e1f43781cf3a7ceba08681cf84699fa32
Inioluwa Oyetunji <theinioye@gmail.com>
2024-09-16 20:31:55 +0100

    Sep 14

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,20 @@
-import express, {Request, Response } from "express"
-import { userLogIn, createUser } from "../controllers/userControllers"
-const userRouter = express.Router()
+import express, { Request, Response } from "express";
+import {
+  userLogIn,
+  createUser,
+  userDashboard,
+} from "../controllers/userControllers";
+const userRouter = express.Router();
 
-import { validateData } from "../middleware/validationMiddleware"
-import { userLogInSchema,userSignUpSchema } from "../schemas/userSchemas"
+import {
+  authenticateToken,
+  validateData,
+} from "../middleware/validationMiddleware";
+import { userLogInSchema, userSignUpSchema } from "../schemas/userSchemas";
 
-userRouter.post("/users",validateData(userSignUpSchema), createUser)
-userRouter.post("/users/logIn", validateData(userLogInSchema), userLogIn)
+userRouter.post("/users", validateData(userSignUpSchema), createUser);
+userRouter.post("/users/logIn", validateData(userLogInSchema), userLogIn);
+userRouter.get("/users/dashboard", authenticateToken, userDashboard);
 
-
-export default userRouter 
+export default userRouter;
+ 
